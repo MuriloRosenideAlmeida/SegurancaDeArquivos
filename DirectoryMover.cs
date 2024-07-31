b0027e3c39bf1316dd822d74204337026e3f18d3
Murilo Almeida <cmdox@gmail.com>
2024-07-30 22:37:20 -0300

    Creação da classe DirectoryMover

---
+++ DirectoryMover.cs
@@ -0,0 +1,20 @@
+﻿namespace SegurancaDeArquivos {
+    internal class DirectoryMover {
+        public static void MoveDirectory( string sourceDirectory, string destinationDirectory) {
+            if (!Directory.Exists(destinationDirectory)) {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+            foreach (string file in Directory.GetFiles(sourceDirectory)) {
+                string fileName = Path.GetFileName(file);
+                string destFile = Path.Combine(destinationDirectory, fileName);
+                File.Move(file, destFile);
+            }
+            foreach (string subdirectory in Directory.GetDirectories(sourceDirectory)) {
+                string subdirectoryName = Path.GetFileName(subdirectory);
+                string destinationSubdirectory = Path.Combine(destinationDirectory, subdirectoryName);
+                File.Move(subdirectory, destinationSubdirectory);
+               // Directory.Delete(subdirectory, true);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,23 +5,22 @@ using System.Collections.Generic;
 namespace SegurancaDeArquivos {
     internal class Program {
         static void Main(string[] args) {
-            string path = @"C:\Users\Dan\OneDrive\Documents\faculdade";
+            string path = @"C:\Users\Dan\OneDrive\Documents\server";
+            string newFolderPath = Path.Combine(path, "securityfolder");
             try {
-                // Tipo inumeravel de string que cria um vetor de pastas contidas do caminho que especifiquei 
-                IEnumerable <string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
-                Console.WriteLine("FOLDERS");
-                foreach (string folder in folders) {
-                    Console.WriteLine(folder);
-                }
-                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
-                Console.WriteLine("FILES");
-                foreach (string file in files) {
-                    Console.WriteLine(file);
-                }
-                Directory.CreateDirectory(path + "\\securityfolder");// Cria a pasta no diretorio que eu especificar
+                Directory.CreateDirectory(newFolderPath);
+                DirectoryMover.MoveDirectory(path, newFolderPath);
+                Console.WriteLine($"Pastas e arquivos movidos/copiedos para {newFolderPath}");
             }
             catch (IOException error) {
                 Console.WriteLine("An error ocurred");
+                Console.WriteLine(error.Message);
+            } 
+            catch (UnauthorizedAccessException error) {
+                Console.WriteLine("Access denied");
+                Console.WriteLine(error.Message);
+            } catch (Exception error) {
+                Console.WriteLine("An unexpected error occurred");
                 Console.WriteLine(error.Message);
             }
         }
